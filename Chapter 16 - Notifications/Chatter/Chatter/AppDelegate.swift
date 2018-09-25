b95a03fcc0885a158bcea194e306a3c544673d94
Darrell Root <darrellrootjunk@mac.com>
2018-09-25 12:09:40 -0700

    Updated Chapter16 to Swift4.2

--- Chapter 16 - Notifications/Chatter/Chatter/AppDelegate.swift
+++ Chapter 16 - Notifications/Chatter/Chatter/AppDelegate.swift
@@ -14,12 +14,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var windowControllers: [ChatWindowController] = []
 
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         addWindowController()
     }
 
     
-    func applicationWillTerminate(aNotification: NSNotification) {
+    func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
     
